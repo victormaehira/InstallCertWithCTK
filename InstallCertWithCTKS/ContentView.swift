18a3a2eacf42b45dea701f804672db26241fb6f9
Victor Yuji Maehira <victor.maehira@certis>
2026-03-26 14:01:28 -0300

    Initial Commit

---
+++ InstallCertWithCTKS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstallCertWithCTKS
+//
+//  Created by victor.maehira on 26/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstallCertWithCTKS/InstallCertWithCTKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstallCertWithCTKSApp.swift
+//  InstallCertWithCTKS
+//
+//  Created by victor.maehira on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct InstallCertWithCTKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
